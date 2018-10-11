bf368a6aef652cac4dcad364b314f9335d1370ab
Senthilkumar <senthil@gmail.com>
2018-10-11 14:22:02 +0530

    Upated gradle project

--- gradlew.bat
+++ gradlew.bat
@@ -14,7 +14,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+set DEFAULT_JVM_OPTS= -Dhttps.proxyHost=proxyvipsgh.nb.ford.com -Dhttps.proxyPort=83 -Dhttp.proxyHost=proxyvipsgh.nb.ford.com -Dhttp.proxyPort=83
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
